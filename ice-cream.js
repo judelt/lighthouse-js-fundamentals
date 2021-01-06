4018383fc3280bc841e76c160a0bbe6840b68a95
Judit Mendez Puig <mendez.puig@gmail.com>
2021-01-06 18:15:32 +0000

    ...

---
+++ ice-cream.js
@@ -0,0 +1,7 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+var newiceCreamFlavours = iceCreamFlavours.push("root beer");
+
+console.log(newiceCreamFlavours)
+
